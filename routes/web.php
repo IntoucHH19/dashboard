8039b434fd2dbb610a12ed38d5a880054abb3143
IntoucHH19 <19306@ps.ac.th>
2023-03-21 15:39:55 +0700

    update project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/profile', [ProfileController::class, 'profile']);
+
+
